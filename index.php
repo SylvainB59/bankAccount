b8474d9730d2bb0f79c61af765441d210773b37f
sylvain <s.baillot59@gmail.com>
2017-11-09 13:46:44 +0100

    add function getAccounts in AccountManager

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
 
-include('controlleurs/index.php');
+if(isset($_POST['deposit']) OR isset($_POST['withdraw']) OR isset($_POST['transfer']))
+{
+	include('controlleurs/banking.php');
+}
+else
+{
+	include('controlleurs/index.php');
+}
+
 
 ?>
---
+++ models/AccountManager.php
@@ -0,0 +1,56 @@
+<?php
+
+class AccountManager
+{
+	public function getBdd()
+	{
+		try
+		{
+			$bdd = new PDO('mysql:host=localhost;dbname=bankAccount;charset=utf8', 'root', '');
+		}
+		catch (Exection $e)
+		{
+			die('Erreur : ' . $e->getMessage());
+		}
+
+		return $bdd;
+	}
+
+	// Get all accounts in db
+	public function getAccounts()
+	{
+		$req = $this->getBdd()->query('SELECT * FROM accounts');
+		$accounts = $req->fetchAll(PDO::FETCH_ASSOC);
+		foreach($accounts as $key =>$value)
+		{
+			$accounts[$key] = new Account($value);
+		}
+
+		return $accounts
+	}
+
+	// Add a new account
+	public function addAccount($account)
+	{
+
+	}
+
+	// Deposit in an account
+	public function addAccount($account)
+	{
+
+	}
+
+	// Withdraw account
+	public function addAccount($account)
+	{
+
+	}
+
+	// transfert beetween accounts
+	public function addAccount($account)
+	{
+
+	}
+
+}
